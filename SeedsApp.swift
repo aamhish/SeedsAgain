2174cb7fdda3ff79a985cf72933e7986a5b4aad1
Aamhish <aamhish@gmail.com>
2024-01-13 17:53:03 -0500

    first commit

---
+++ SeedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeedsApp.swift
+//  Seeds
+//
+//  Created by Aamhish Rao on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SeedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
